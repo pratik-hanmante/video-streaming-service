d58894b605d720b5ebd8d73b912d1b5ce320a535
pratik <pratikhanmante3@gmail.com>
2026-05-11 23:11:51 +0530

    updating movie properties

--- content-service/src/main/java/com/netflix/contentservice/model/Movie.java
+++ content-service/src/main/java/com/netflix/contentservice/model/Movie.java
@@ -8,11 +8,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,58 +26,38 @@ public class Movie {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(updatable = false, nullable = false)
     private String id;
 
-    @NotBlank
-    @Column(nullable = false)
     private String title;
 
-    @NotBlank
-    @Column(nullable = false, columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT")
     private String description;
 
-    @NotNull
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private Genre genre;
 
-    @NotBlank
-    @Column(nullable = false)
     private String director;
 
     @Column(columnDefinition = "TEXT")
     private String cast;
 
-    @Min(1888)
-    @Column(nullable = false)
     private int releaseYear;
 
-    @DecimalMin("0.0")
-    @DecimalMax("10.0")
-    @Column(nullable = false)
     private double rating;
 
-    @Column(name = "thumbnail_url")
     private String thumbnailUrl;
 
-    @Min(1)
-    @Column(nullable = false)
     private int durationMinutes;
 
     // S3 key for video file
-    @Column(name = "video_key")
     private String videoKey;
 
     // HLS URL playlist for streaming
-    @Column(name = "hls_url")
     private String hlsUrl;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private VideoStatus videoStatus;
 
     @CreationTimestamp
-    @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 }
