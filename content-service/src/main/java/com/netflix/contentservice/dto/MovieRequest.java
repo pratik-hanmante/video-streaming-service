323705dadaed88a62b756875c445c418f851a56e
pratik-hanmante <84140896+pratik-hanmante@users.noreply.github.com>
2026-06-01 23:57:22 +0530

    Update MovieRequest.java

--- content-service/src/main/java/com/netflix/contentservice/dto/MovieRequest.java
+++ content-service/src/main/java/com/netflix/contentservice/dto/MovieRequest.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MovieRequest {
 
-    @NotBlank(message = "title is required")
+    @NotBlank(message = "title is required to enter")
     private String title;
 
     private String description;
